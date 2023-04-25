ea85698c14d56cb692fdbe844329cdb82e04c2ad
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-04-25 17:49:54 -0300

    first commit

---
+++ mudi/src/main/java/br/com/mudi/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,53 @@
+package br.com.mudi.mvc.mudi.dto;
+
+import br.com.mudi.mvc.mudi.model.Pedido;
+import br.com.mudi.mvc.mudi.model.StatusPedido;
+import jakarta.validation.constraints.NotBlank;
+
+public class RequisicaoNovoPedido {
+
+	@NotBlank//NotBlank.requisicaoNovoPedido.nomeProduto=O campo nome do produto é obrigatório
+	private String nomeProduto;
+	
+	@NotBlank(message = "O campo url do produto é obrigatório")
+	private String urlProduto;
+	
+	@NotBlank
+	private String urlImagem;
+	private String descricao;
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setDescricao(descricao);
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setUrlImagem(urlImagem);
+		pedido.setUrlProduto(urlProduto);
+		pedido.setStatus(StatusPedido.AGUARDANDO);
+		return pedido;
+	}
+	
+}
